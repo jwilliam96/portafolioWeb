6841280b5f3d40ab74ffc54906694867d58154ec
jwilliam96 <jwsa.1996@gmail.com>
2025-04-21 22:14:54 -0500

    creando carpetas

---
+++ src/ui/TopMenu/TopMenu.tsx
@@ -0,0 +1,13 @@
+
+export default function TopMenu() {
+    return (
+        <nav>
+            <ul>
+                <li>Sobre mi</li>
+                <li>Proyectos</li>
+                <li>Habilidades</li>
+                <li>Contacto</li>
+            </ul>
+        </nav>
+    )
+}
